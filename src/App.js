09fd6408ab1563105b80898455327eb17d37c55d
chrisdionisius <chrisdionisius@gmail.com>
2023-04-18 18:23:00 +0700

    downgrade react dom & add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
-  return <div className="App">Hi there</div>;
+  return (
+    <div className="App">
+      <Router>
+        {/* <Switch> */}
+        <Route path="/" component={LandingPage}></Route>
+        {/* </Switch> */}
+      </Router>
+    </div>
+  );
 }
 
 export default App;
